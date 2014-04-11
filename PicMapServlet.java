59ffd944471aabd7fd3dd39425d48b522e378bf7
synaptique <anass05elhamidi@gmail.com>
2014-04-11 09:20:38 +0100

    Create PicMapServlet.java

---
+++ PicMapServlet.java
@@ -0,0 +1,28 @@
+package picmap;
+
+import java.io.IOException;
+
+import javax.servlet.http.*;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+@SuppressWarnings("serial")
+public class PicMapServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		UserService userService = UserServiceFactory.getUserService(); 
+        User user = userService.getCurrentUser();          
+            if (user == null) 
+            { 
+            	resp.sendRedirect(userService.createLoginURL(req.getRequestURI())); 
+                 
+            } 
+            else {
+            resp.sendRedirect("Main.html");
+             
+            }
+            
+	}
+}
